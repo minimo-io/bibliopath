d527904f06942c5a0e0c5ee7e2b174ea4e4193f1
nicolas-ea <nicolas@minimo.io>
2020-07-02 09:55:31 -0300

    Top margins.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
   <ion-content padding>
-    <ion-searchbar></ion-searchbar>
-    <router-link :to="{ path: '/book/seneca' }">
-      Seneca's Morals of a Happy Life, Benefits, Anger and Clemency
-    </router-link>
-    The world is your oyster.
-    <p>Heyyy esto es lo que se llama el primitivo inicio de Bibliopath, la editorial abierta.</p>
-    <ion-button @click="goToAbout" full>Go to About</ion-button>
-
+     <ion-grid fixed>
+      <ion-searchbar></ion-searchbar>
+      <router-link :to="{ path: '/book/seneca' }">
+        Seneca's Morals of a Happy Life, Benefits, Anger and Clemency
+      </router-link>
+      The world is your oyster.
+      <p>Heyyy esto es lo que se llama el primitivo inicio de Bibliopath, la editorial abierta.</p>
+      <ion-button @click="goToAbout" full>Go to About</ion-button>
+    </ion-grid>
 
   </ion-content>
 </template>
