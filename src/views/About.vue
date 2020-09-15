7b689f7ecf66d9d11eee150aba482e3340c6490c
nicolas-ea <nicolas@minimo.io>
2020-09-15 13:47:32 -0300

    More about page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,32 +1,84 @@
 <template>
-  <div class="about container">
-    <b-card class="book-header pb-4 pt-0 pt-md-3 pb-md-5 mb-4 mb-sm-0" no-body>
+  <div class="about container pt-0">
+    <div class="container primary-container pt-0 px-0 mt-3 mt-lg-5" no-body>
 
-      <b-card-body class="z-index-6000">
-        <div class="book-presentation pt-4">
-          <h1>
-            About us
-          </h1>
-          <div class="author-meta mt-2">
-            <!-- by <b-avatar variant="light" size="2.2rem" :src="book.author.avatar" class="mr-2 ml-2"></b-avatar> -->
-            <!-- <span class="mr-auto"><b-link :to="{ name: 'Author', params: { slug: book.author.slug } }">{{ book.author.name }}</b-link></span> -->
-          </div>
+      <header class="alt-post-margins alt-top-title mx-0">
+        <b-link class="breadcrumb-back-button">
+          <i class="fas fa-reply"></i>&nbsp;back
+        </b-link>
+
+        <h1 class="post-title category-title" itemprop="name headline">
+          <span>About us</span>
+          <span class='app-text-green'>.</span>
+        </h1>
+
+        <div class="post-subtitle category-subtitle mt-3">
+
+          <div role="alert" aria-live="polite" aria-atomic="true" class="alert alert-warning">
+            <!----><span><strong>Bibliopath</strong> in under development.</span><span>
+            Meanwhile we invite you to find out more about us in our account <a href="https://www.instagram.com/hopmasters/" rel="noopener" target="_blank" class="text-nowrap"> <i class="fab fa-patreon mr-1"></i>Patreon</a> account. </span></div>
+
+          <figure class="figure">
+            <b-img src="@/assets/andrew-neel--FVaZbu6ZAE-unsplash.jpg" rounded fluid alt="book-image-unsplash"></b-img>
+            <figcaption class="figure-caption">Thanks to Andrew Neel</figcaption>
+          </figure>
+
+
+          <b-link href="#what" class="app-title Xapp-text-selected mt-4"><i class="fas fa-hashtag"></i> WHAT IN THE WORLD...?</b-link>
+          <p class="mt-2">
+            Bibliopath is an editorial project promoting Open reading & Open publishing.
+            <br>
+            If you are a visitor you will be able ro read, listen & discuss books based on Open Standards.
+            If you are an author <a href="#" rel="noopener" target="_blank" class=""><span class="text-nowrap"><i class="fas fa-upload mr-1"></i>Publish for free</span></a> and get some <a href="/#" class="router-link-exact-active router-link-active" target="_self" aria-current="page"><i class="fas fa-hand-holding-usd"></i></a> and <a href="/#" class="router-link-exact-active router-link-active" target="_self" aria-current="page"><i class="fas fa-heart"></i></a> from your followers.
+          </p>
+          <p>
+            We focus on having good quality public domain works & contemporary authors that see a potential in an open publishing model.
+
+          </p>
+          <p>Our goal is to create a community of authors and readers that can grow based on Open Standards.</p>
+
+
+          <b-link href="#why" class="app-title Xapp-text-selected mt-4"><i class="fas fa-hashtag"></i> HOW DOES THIS THING WORK?</b-link>
+
+          <p class="mt-2">
+            Bibliopath has 4 types of users:
+          </p>
+
+          <p>
+            <ol class='beer-list'>
+              <li>
+                <b-link to="#" class="Xbg-black"><i class="fas fa-mask mr-1"></i>ANONYMOUS</b-link>
+                 Can read any book of the library & share fragments.
+              </li>
+              <li>
+                <b-link to="#"><i class="fas fa-user mr-1"></i>USERS</b-link>
+                After signin up for free can download books for offline reading. Listen to available audiobooks. Follow authors. Highlight & comment fragments & participate in discussions + What anonymous user can do.
+              </li>
+              <li>
+                <b-link to="#"><i class='fas fa-graduation-cap mr-1'></i>BIBLIOPATHS</b-link>
+                These are the Patreon donors to the project. Bibliopaths can access to featured books & authors -published by Bibliopath- and get free paper books + What normal user can do.
+
+              </li>
+              <li>
+                <b-link to="#"><i class="fas fa-pen-fancy mr-1"></i>AUTHORS</b-link>
+                Get to freely distribute their work through the platform! Get donors & followers and the ability to interact with their readers base.
+              </li>
+            </ol>
+          </p>
+
+
+          <b-link href="#who" class="app-title Xapp-text-selected mt-4"><i class="fas fa-hashtag"></i> WHO ARE YOU NUTS?</b-link>
+          <p class="mt-2">A gang of readers with (some) programming skills <i class="fas fa-grin-beam-sweat"></i>
+          <p>
+
+            In fact, Bibliopath was developed by <b-link href="https://minimo.io/en/" target="_blank" class="text-nowrap"><i class="fas fa-circle mr-1"></i>Mínimo</b-link>,
+            a digital ideas laboratory based in Uruguay, with the initiative of contributing to the open world.
+          </p>
         </div>
-        <!-- <img :src="book.presentation" /> -->
-        <!-- <div class="book-meta softer mb-3 text-center text-sm-left">
-          <b-link href="#"><b-button variant="danger" size="sm" class="mr-2"><i class="far fa-heart mr-1"></i>Like<b-badge pill variant="light" class="ml-2 softer">252</b-badge></b-button></b-link>
-          <b-link href="#"><b-button variant="info" size="sm" class="mr-2"><i class="fas fa-plus mr-1"></i>Info</b-button></b-link>
-          <b-link href="#"><b-button variant="green" size="sm"><i class="fas fa-user mr-1"></i>Bio</b-button></b-link>
-        </div> -->
-        <!-- <div class="book-buy text-center">
-          <b-link href="#" target="_blank" class="alt-aff-link softer">
-            <i class="fab fa-amazon mr-1"></i>Comprar libro de bolsillo</b-link>
-        </div> -->
-      </b-card-body>
-      <!-- <div class="overlay overlay-dark"></div> -->
+      </header>
 
-    </b-card>
-    <div class="book-box mt-2">
+    </div>
+    <!-- <div class="book-box mt-2">
 
       <div class="book-content mt-5 padding-right-inherit">
         <p>Bibliopath es un proyecto editorial que promueve la lectura y la publicación abierta.</p>
@@ -42,6 +94,6 @@
         </ul>
       </div>
 
-    </div>
+    </div> -->
   </div>
 </template>
