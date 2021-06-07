a3c598948d7a6da9a40a24ca11b18b6e0920b362
minimo-io <nicolas@minimo.io>
2021-06-06 21:07:52 -0300

    get it ready for upload to blog.minimo.io

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,41 @@
 // vue.config.js
 module.exports = {
-  configureWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      // mutate config for production...
-    } else {
-      // mutate for development...
-    }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/bibliopath/'
+    : '/'
+  ,
+  configureWebpack: {
+     optimization: {
+       splitChunks: false
+     },
+     output: {
+        filename: "js/bibliopath.js"
+     },
   },
   chainWebpack: config => {
+    config.optimization.delete('splitChunks') // no vendor chunks
+    config.plugins.delete('prefetch')         // no prefetch chunks
+    config.plugins.delete('preload')          // no preload chunks
+
+    if (config.plugins.has("extract-css")) {
+      const extractCSSPlugin = config.plugin("extract-css");
+      extractCSSPlugin &&
+        extractCSSPlugin.tap(() => [
+          {
+            filename: "css/bibliopath.css",
+            chunkFilename: "css-bibliopath-[name].css"
+          }
+        ]);
+    }
+  }
+  // configureWebpack: config => {
+  //   if (process.env.NODE_ENV === 'production') {
+  //     // mutate config for production...
+  //   } else {
+  //     // mutate for development...
+  //   }
+  // },
+  //chainWebpack: config => {
     // config.module.rule('epub')
     //   .test(/\.(epub)(\?.*)?$/)
     //   .use('file-loader')
@@ -16,6 +44,6 @@ module.exports = {
     //       name: 'assets/books/[name].[hash:8].[ext]'
     // });
 
-  },
+  //},
 
 }
