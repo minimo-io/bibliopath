9eb7fcff66aca737f0835d3bd9494a25a0b16c9c
Nicolas Erramuspe <nicolas@minimo.io>
2025-07-31 19:47:03 -0300

    new actions in list view

--- src/lib/components/BookList.svelte
+++ src/lib/components/BookList.svelte
@@ -58,31 +58,122 @@
 							{/if}
 						</div>
 					</td>
-					<td class="flex flex-col items-center gap-1">
-						<a href={generateBookUrl(book)} class="btn btn-primary btn-xs">
-							<BookOpen size={16} />
-							{book.lastRead || book.lastAccessed ? 'Continue' : 'Start'}
-						</a>
-						{#if showActions}
-							<a
-								href={book.url}
-								target="_blank"
-								rel="noopener noreferrer"
-								class="btn btn-ghost btn-xs btn-square"
-								title="Open original source"
-							>
-								<ExternalLink size={16} />
-							</a>
-							{#if handleRemoveBook}
-								<button
-									class="btn btn-ghost btn-xs btn-square"
-									onclick={() => handleRemoveBook(book)}
-									title="Remove from library"
+					<td class="text-right">
+						<div class="dropdown dropdown-left">
+
+							<div tabindex="0" role="button" class="btn btn-ghost btn-xs btn-square">
+
+								<svg
+
+									xmlns="http://www.w3.org/2000/svg"
+
+									width="24"
+
+									height="24"
+
+									viewBox="0 0 24 24"
+
+									fill="none"
+
+									stroke="currentColor"
+
+									stroke-width="2"
+
+									stroke-linecap="round"
+
+									stroke-linejoin="round"
+
+									class="lucide lucide-more-vertical"
+
+									><circle cx="12" cy="12" r="1" /><circle cx="12" cy="5" r="1" /><circle
+
+										cx="12"
+
+										cy="19"
+
+										r="1"
+
+									/></svg
+
 								>
-									<Trash2 size={14} />
-								</button>
-							{/if}
-						{/if}
+
+							</div>
+
+							<ul
+
+								tabindex="0"
+
+								class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+
+							>
+
+								<li>
+
+									<a href={generateBookUrl(book)} class="btn btn-primary btn-xs">
+
+										<BookOpen size={16} />
+
+										{book.lastRead || book.lastAccessed ? 'Continue' : 'Start'}
+
+									</a>
+
+								</li>
+
+								{#if showActions}
+
+									<li>
+
+										<a
+
+											href={book.url}
+
+											target="_blank"
+
+											rel="noopener noreferrer"
+
+											class="btn btn-ghost btn-xs"
+
+											title="Open original source"
+
+										>
+
+											<ExternalLink size={16} />
+
+											Open original source
+
+										</a>
+
+									</li>
+
+									{#if handleRemoveBook}
+
+										<li>
+
+											<button
+
+												class="btn btn-ghost btn-xs"
+
+												onclick={() => handleRemoveBook(book)}
+
+												title="Remove from library"
+
+											>
+
+												<Trash2 size={14} />
+
+												Remove from library
+
+											</button>
+
+										</li>
+
+									{/if}
+
+								{/if}
+
+							</ul>
+
+						</div>
 					</td>
 				</tr>
 			{/each}
